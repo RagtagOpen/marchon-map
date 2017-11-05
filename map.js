ce99379a0a38ba62fbc62748c8123d2cc5990856
Kimberly Nicholls <kimberly@gridium.com>
2017-11-05 10:34:20 -0800

    zoom to user location if available

--- map.js
+++ map.js
@@ -6,9 +6,19 @@ const app = new Vue({
     activeGroup: null,
     map: null,
     popup: {},
+    userLocation: null,
+    mapLoaded: false,
   },
 
-  mounted: function created() {
+  created: function created() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.userLocation = position.coords;
+      });
+    }
+  },
+
+  mounted: function mounted() {
     this.map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/march-on/cj95y50am1fbj2rqhogye1lu5',
@@ -21,20 +31,68 @@ const app = new Vue({
       closeOnClick: false,
     });
     this.map.on('load', () => {
-      this.map.on('click', 'marchon-sheet', e => this.showDetails(e));
-      this.map.on('mousemove', 'marchon-sheet', _.throttle(e => this.showDetails(e), 100));
-      this.map.on('mouseenter', 'marchon-sheet', e => this.showPopup(e));
-      this.map.on('mouseleave', 'marchon-sheet', e => this.hidePopup(e));
+      this.mapLoaded = true;
+      this.map.on('click', 'marchon-sheet', e => this.showFeature(e.features[0]));
+      this.map.on('mousemove', 'marchon-sheet', _.throttle(e => this.showFeature(e.features[0]), 100));
+      this.map.on('mouseenter', 'marchon-sheet', e => this.showPopup(e.features[0]));
+      this.map.on('mouseleave', 'marchon-sheet', e => this.hidePopup(e.features[0]));
     });
   },
 
-  methods: {
-    showDetails: function showDetails(e) {
-      if (!e.features || !e.features.length) {
-        return;
+  watch: {
+    mapLoaded: function mapLoaded() {
+      if (this.userLocation) {
+        this.zoomToClosest();
       }
+    },
 
-      const feature = e.features[0];
+    userLocation: function userLocation() {
+      if (this.mapLoaded) {
+        this.zoomToClosest();
+      }
+    },
+  },
+
+  methods: {
+    zoomToClosest() {
+      const features = this.map.queryRenderedFeatures({ layers: ['marchon-sheet'] });
+      const loc = this.userLocation;
+      const withDistance = features.map((feature) => {
+        const coords = feature.geometry.coordinates;
+
+        return {
+          feature,
+          distance: this.distance(loc.latitude, loc.longitude, coords[1], coords[0]),
+        };
+      });
+      const closest = _.minBy(withDistance, d => d.distance);
+      const featureLoc = closest.feature.geometry.coordinates; // [lng, lat]
+
+      this.map.fitBounds([
+        // sw
+        [Math.min(loc.longitude, featureLoc[0]), Math.min(loc.latitude, featureLoc[1])],
+        // ne
+        [Math.max(loc.longitude, featureLoc[0]), Math.max(loc.latitude, featureLoc[1])],
+      ],
+      { maxZoom: 15, padding: 50 });
+      this.showFeature(closest.feature);
+      this.showPopup(closest.feature);
+    },
+
+    deg2rad: deg => deg * (Math.PI / 180),
+
+    distance: function distance(lat1, lon1, lat2, lon2) {
+      // distance in km
+      const dLat = this.deg2rad(lat2 - lat1);
+      const dLon = this.deg2rad(lon2 - lon1);
+      const a = (Math.sin(dLat / 2) * Math.sin(dLat / 2)) +
+          (Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+           Math.sin(dLon / 2) * Math.sin(dLon / 2));
+
+      return 6371 * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    },
+
+    showFeature: function showFeature(feature) {
       const props = feature.properties;
 
       props.mailto = `mailto:${props['contact email']}`;
@@ -42,13 +100,7 @@ const app = new Vue({
       this.activeGroup = props;
     },
 
-    showPopup: function showPopup(e) {
-      if (!e.features || !e.features.length) {
-        return;
-      }
-
-      const feature = e.features[0];
-
+    showPopup: function showPopup(feature) {
       this.popup
         .setLngLat(feature.geometry.coordinates)
         .setHTML(`<b>${feature.properties.name}</b>`)
@@ -60,10 +112,3 @@ const app = new Vue({
     },
   },
 });
-
-/*
-map.on('mousemove', 'marchon-sheet', _.throttle(function(e) {
-  console.log('mousemove', e.point);
-}, 100));
-
-*/
