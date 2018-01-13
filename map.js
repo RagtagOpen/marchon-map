e2c7367735a5adfb6a427eee8b7c873c3fc652d6
Kimberly Nicholls <kimberly@gridium.com>
2018-01-13 14:54:36 -0800

    house parties in the middle

--- map.js
+++ map.js
@@ -143,16 +143,6 @@ const app = new Vue({
 
         // add the map layers to the map, and also to the vue mapLayers
         // data. Not currently using initiallyChecked.
-        if (sourceActionNetwork.features.length) {
-          _this.map.addSource('marchon-source-actionnetwork-geojson', { type: 'geojson', data: sourceActionNetwork });
-          _this.addLayer('marchon-source-actionnetwork', 'marchon-source-actionnetwork-geojson', 'house');
-          _this.mapLayers.push({
-            layerId: 'marchon-source-actionnetwork',
-            label: 'House Parties',
-            icon: 'house.svg',
-            initiallyChecked: true,
-          });
-        }
         if (affiliateFalse.features.length) {
           _this.map.addSource('marchon-affiliate-false-geojson', { type: 'geojson', data: affiliateFalse });
           _this.addLayer('marchon-affiliate-false', 'marchon-affiliate-false-geojson', 'star-15-red');
@@ -160,6 +150,16 @@ const app = new Vue({
             layerId: 'marchon-affiliate-false',
             label: 'Non Affiliates',
             icon: 'star-15-red.svg',
+            initiallyChecked: true,
+          });
+        }
+        if (sourceActionNetwork.features.length) {
+          _this.map.addSource('marchon-source-actionnetwork-geojson', { type: 'geojson', data: sourceActionNetwork });
+          _this.addLayer('marchon-source-actionnetwork', 'marchon-source-actionnetwork-geojson', 'house');
+          _this.mapLayers.push({
+            layerId: 'marchon-source-actionnetwork',
+            label: 'House Parties',
+            icon: 'house.svg',
             initiallyChecked: true,
           });
         }
